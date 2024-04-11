005c1bd8e9ed886608d1c99bd630545b9824fe94
Zachary Handley <zachhandley@gmail.com>
2024-04-10 17:46:17 -0700

    fixed examples, added more examples

---
+++ example/appwrite/customDefinitions.ts
@@ -0,0 +1,11 @@
+import type { ConverterFunctions, ValidationRules, AfterImportActions } from "appwrite-utils";
+
+export const customConverterFunctions: ConverterFunctions = {
+  // Add your custom converter functions here
+}
+export const customValidationRules: ValidationRules = {
+  // Add your custom validation rules here
+}
+export const customAfterImportActions: AfterImportActions = {
+  // Add your custom after import actions here
+}
