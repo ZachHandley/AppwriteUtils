f58e794db5e2635a13a21ef6c81eb2a169d3bc62
ZachHandley <zachhandley@gmail.com>
2024-10-20 14:26:14 -0700

    Added getFilePreviewUrl

--- packages/appwrite-utils/src/functions/files.ts
+++ packages/appwrite-utils/src/functions/files.ts
@@ -43,3 +43,58 @@ export const getFileDownloadUrl = (
     jwt ? `&jwt=${jwt.jwt}` : ""
   }`;
 };
+
+/**
+ * Generates a preview URL for a file based on the provided endpoint, project ID, bucket ID, file ID, and optional JWT.
+ * Only works on Image Files with a max of 10 MB file size.
+ *
+ * @param {string} endpoint - The base URL endpoint.
+ * @param {string} projectId - The ID of the project.
+ * @param {string} bucketId - The ID of the bucket.
+ * @param {string} fileId - The ID of the file.
+ * @param {Models.Jwt} [jwt] - Optional JWT object for authentication with Appwrite.
+ * @param {Object} [options] - Optional options for the preview
+ * @param {number} [options.width] - The width of the preview
+ * @param {number} [options.height] - The height of the preview
+ * @param {string} [options.gravity] - The gravity of the preview
+ * @param {number} [options.quality] - The quality of the preview
+ * @param {number} [options.borderWidth] - The border width of the preview
+ * @param {string} [options.borderColor] - The border color of the preview
+ * @param {number} [options.borderRadius] - The border radius of the preview
+ * @param {number} [options.opacity] - The opacity of the preview
+ * @param {number} [options.rotation] - The rotation of the preview
+ * @param {string} [options.background] - The background of the preview
+ * @param {string} [options.output] - The output format of the preview
+ * @return {string} The complete preview URL for the file.
+ */
+export const getFilePreviewUrl = (
+  endpoint: string,
+  projectId: string,
+  bucketId: string,
+  fileId: string,
+  jwt?: Models.Jwt,
+  options?: {
+    width?: number;
+    height?: number;
+    gravity?: string;
+    quality?: number;
+    borderWidth?: number;
+    borderColor?: string;
+    borderRadius?: number;
+    opacity?: number;
+    rotation?: number;
+    background?: string;
+    output?: string;
+  },
+) => {
+  const queryParams = new URLSearchParams();
+  if (options) {
+    Object.entries(options).forEach(([key, value]) => {
+      if (value !== undefined && value !== null)
+        queryParams.set(key, value.toString());
+    });
+  }
+  return `${endpoint}/storage/buckets/${bucketId}/files/${fileId}/preview?project=${projectId}${
+    jwt ? `&jwt=${jwt.jwt}` : ""
+  }${queryParams.toString()}`;
+};
